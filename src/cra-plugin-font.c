468f4fad61a61890d955f576871f6f83376a9cbf
Richard Hughes <richard@hughsie.com>
2014-03-07 15:26:51 +0000

    Add the start of font plugin

---
+++ src/cra-plugin-font.c
@@ -0,0 +1,124 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2014 Richard Hughes <richard@hughsie.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <config.h>
+#include <fnmatch.h>
+
+#include <cra-plugin.h>
+
+/**
+ * cra_plugin_get_name:
+ */
+const gchar *
+cra_plugin_get_name (void)
+{
+	return "font";
+}
+
+/**
+ * cra_plugin_add_globs:
+ */
+void
+cra_plugin_add_globs (CraPlugin *plugin, GPtrArray *globs)
+{
+	cra_plugin_add_glob (globs, "/usr/share/fonts/*/*.otf");
+	cra_plugin_add_glob (globs, "/usr/share/fonts/*/*.ttf");
+}
+
+/**
+ * _cra_plugin_check_filename:
+ */
+static gboolean
+_cra_plugin_check_filename (const gchar *filename)
+{
+	if (fnmatch ("/usr/share/fonts/*/*.otf", filename, 0) == 0)
+		return TRUE;
+	if (fnmatch ("/usr/share/fonts/*/*.ttf", filename, 0) == 0)
+		return TRUE;
+	return FALSE;
+}
+
+/**
+ * cra_plugin_check_filename:
+ */
+gboolean
+cra_plugin_check_filename (CraPlugin *plugin, const gchar *filename)
+{
+	return _cra_plugin_check_filename (filename);
+}
+
+/**
+ * cra_plugin_process_filename:
+ */
+static gboolean
+cra_plugin_process_filename (CraPlugin *plugin,
+			     CraPackage *pkg,
+			     const gchar *filename,
+			     GList **apps,
+			     const gchar *tmpdir,
+			     GError **error)
+{
+	return TRUE;
+}
+
+/**
+ * cra_plugin_process:
+ */
+GList *
+cra_plugin_process (CraPlugin *plugin,
+		    CraPackage *pkg,
+		    const gchar *tmpdir,
+		    GError **error)
+{
+	gboolean ret;
+	GList *apps = NULL;
+	guint i;
+	gchar **filelist;
+
+	filelist = cra_package_get_filelist (pkg);
+	for (i = 0; filelist[i] != NULL; i++) {
+		if (!_cra_plugin_check_filename (filelist[i]))
+			continue;
+		ret = cra_plugin_process_filename (plugin,
+						   pkg,
+						   filelist[i],
+						   &apps,
+						   tmpdir,
+						   error);
+		if (!ret) {
+			/* FIXME: free apps? */
+			apps = NULL;
+			goto out;
+		}
+	}
+
+	/* no fonts files we care about */
+	if (apps == NULL) {
+		g_set_error (error,
+			     CRA_PLUGIN_ERROR,
+			     CRA_PLUGIN_ERROR_FAILED,
+			     "nothing interesting in %s",
+			     cra_package_get_filename (pkg));
+		goto out;
+	}
+out:
+	return apps;
+}
