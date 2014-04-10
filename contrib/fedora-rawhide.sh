f8ffd6e45522f58c049963231b960c4313e4237d
Richard Hughes <richard@hughsie.com>
2014-04-10 17:59:31 +0100

    trivial: Add two helper scripts for Fedora

---
+++ contrib/fedora-rawhide.sh
@@ -0,0 +1,12 @@
+#sudo mount -t tmpfs -o size=1G,nr_inodes=5k tmpfs /mnt/
+time src/createrepo_as \
+	--log-dir=../createrepo_as_logs \
+	--temp-dir=./contrib/tmp \
+	--cache-dir=./contrib/cache \
+	--packages-dir=../fedora-appstream/fedora-rawhide/packages/ \
+	--extra-appstream-dir=../fedora-appstream/appstream-extra \
+	--extra-appdata-dir=../fedora-appstream/appdata-extra \
+	--extra-screenshots-dir=../fedora-appstream/screenshots-extra \
+	--output-dir=./contrib \
+	--basename=fedora-21 \
+	--screenshot-uri=http://alt.fedoraproject.org/pub/alt/screenshots/f21/
