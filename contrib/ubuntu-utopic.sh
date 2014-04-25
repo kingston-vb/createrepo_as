af606f9ab664ee4554f6ff8a94810ca5932be6f2
Richard Hughes <richard@hughsie.com>
2014-04-25 09:25:42 +0100

    trivial: Add helper to generate the metadata on Ubuntu Utopic

---
+++ contrib/ubuntu-utopic.sh
@@ -0,0 +1,8 @@
+time src/createrepo_as \
+	--log-dir=./contrib/logs-ubu \
+	--temp-dir=./contrib/tmp-ubu \
+	--cache-dir=./contrib/cache-ubu \
+	--packages-dir=/media/raid/Archive/Ubuntu/ \
+	--extra-appdata-dir=../fedora-appstream/appdata-extra \
+	--output-dir=./contrib \
+	--basename=ubuntu-utopic
