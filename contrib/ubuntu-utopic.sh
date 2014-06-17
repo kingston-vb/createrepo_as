3f4da5c63b2bfe9a31bbc203177178fcd64eef31
Richard Hughes <richard@hughsie.com>
2014-06-17 12:31:45 +0100

    Merge project code with libappstream-glib

--- contrib/ubuntu-utopic.sh
+++
@@ -1,8 +0,0 @@
-time src/createrepo_as \
-	--log-dir=./contrib/logs-ubu \
-	--temp-dir=./contrib/tmp-ubu \
-	--cache-dir=./contrib/cache-ubu \
-	--packages-dir=/media/raid/Archive/Ubuntu/ \
-	--extra-appdata-dir=../fedora-appstream/appdata-extra \
-	--output-dir=./contrib \
-	--basename=ubuntu-utopic
